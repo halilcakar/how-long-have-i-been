9b49d15012980f4830868781c6992b51d2473a6b
halilcakar <hcakar.1992@gmail.com>
2020-06-22 17:43:09 +0300

    small updates

--- src/components/VueMain.vue
+++ src/components/VueMain.vue
@@ -8,38 +8,36 @@
 
     <div class="input-group justify-content-center pb-3">
       <div class="input-group-prepend">
-        <select class="input-group-text" v-model.number="day">
+        <select class="input-group-text" v-model.number="selectedDate.day">
           <option v-for="i in days" :key="i">{{ i }}</option>
         </select>
       </div>
-      <select class="select w-50" v-model.number="month">
+      <select class="select w-50" v-model.number="selectedDate.month">
         <option v-for="(month, mIndex) in months" :value="mIndex" :key="mIndex">{{ month }}</option>
       </select>
       <div class="input-group-append">
-        <select class="input-group-text" v-model.number="year">
+        <select class="input-group-text" v-model.number="selectedDate.year">
           <option v-for="(year, yIndex) in years" :key="yIndex">{{ year }}</option>
         </select>
       </div>
     </div>
 
-    <div class="input-group justify-content-center align-items-center pb-3">
+    <div class="input-group justify-content-center align-items-center">
       <div class="input-group-prepend">
-        <select class="input-group-text" v-model.number="hour">
+        <select class="input-group-text" v-model.number="selectedDate.hour">
           <option v-for="(hours, hIndex) in hours" :key="hIndex">{{ hours }}</option>
         </select>
       </div>
       <span class="px-1">:</span>
       <div class="input-group-append">
-        <select class="input-group-text" v-model.number="minute">
+        <select class="input-group-text" v-model.number="selectedDate.minute">
           <option v-for="(minutes, mIndex) in minutes" :key="mIndex">{{ minutes }}</option>
         </select>
       </div>
     </div>
 
-    <hr class="w-100 pb-3" />
-
-    <div id="details">
-      <p>{{ $t("yearsOld", { age: age, day: getDay }) }}</p>
+    <div id="details" class="border border-info my-4 p-3 shadow">
+      <p v-html="$t('yearsOld', { age: age, day: getDay })"></p>
       <p v-html="$t('haveBenn.months', { monthsPassed })"></p>
       <p v-html="$t('haveBenn.weeks', { weeksPassed })"></p>
       <p v-html="$t('haveBenn.days', { daysPassed })"></p>
@@ -48,12 +46,10 @@
       <p v-html="$t('haveBenn.seconds', { secondsPassed })"></p>
     </div>
 
-    <hr class="w-100 pb-3" />
+    <h4 v-html="$t('next', { nextBirthday }) + ' 💖'"></h4>
 
-    <div>
-      <p>{{ `${$t("next", { nextBirthday })}` }}</p>
-    </div>
-    <h5 class="text-muted">Long live and prosper!</h5>
+    <h5 class="text-muted mt-1">Long live and prosper!!</h5>
+    <!-- <h5 class="text-muted">Yet in the darkness, there will be light..</h5> -->
   </main>
 </template>
 
@@ -62,14 +58,16 @@ export default {
   name: "VueMain",
   data() {
     return {
-      year: 1900,
+      selectedDate: {
+        year: 1900,
+        month: 0,
+        day: 1,
+        minute: 0,
+        hour: 0
+      },
       years: [],
-      month: 0,
       days: [],
-      day: 1,
       hours: [],
-      hour: 0,
-      minute: 0,
       minutes: [],
       now: Date.now(),
       birthdate: null
@@ -89,24 +87,21 @@ export default {
       }
       this.years.push(i + 1899);
     }
+    this.years.reverse();
     this.hours.unshift(0);
     this.minutes.unshift(0);
-    this.month = date.getMonth() + 1;
-    this.year = date.getFullYear();
-    this.day = date.getDate();
-    this.hour = 0;
-    this.minute = 0;
 
-    //test
-    this.year = 1992;
-    this.month = 8;
-    this.day = 2;
+    // test with my own birthday :)
+    this.selectedDate.day = 2;
+    this.selectedDate.month = 8;
+    this.selectedDate.year = 1992;
+
     this.birthdate = new Date(
-      this.year,
-      this.month,
-      this.day,
-      this.hour,
-      this.minute
+      this.selectedDate.year,
+      this.selectedDate.month,
+      this.selectedDate.day,
+      this.selectedDate.hour,
+      this.selectedDate.minute
     );
 
     this.now = Date.now();
@@ -124,34 +119,24 @@ export default {
     weekDays() {
       return this.$t("weekDays");
     },
-
     getDay() {
-      return this.weekDays[this.birthdate.getDate() % 7];
+      return this.weekDays[this.birthdate.getDay()];
     },
     age() {
-      return new Date().getFullYear() - this.year - 1;
+      return new Date().getFullYear() - this.selectedDate.year;
     },
+
     diff() {
       return this.now - this.birthdate.getTime();
     },
-    nextBirthday() {
-      let date = new Date();
-      let currentBirthday = new Date(
-        date.getFullYear(),
-        this.month,
-        this.day,
-        this.hour,
-        this.minute
-      );
-
-      let diff = currentBirthday - date;
-      if (currentBirthday < date) {
-        diff = date - currentBirthday;
-      }
-      return Math.round(diff / (24 * 60 * 60 * 1000));
-    },
     monthsPassed() {
-      return this.format(this.age * 12 + (new Date().getMonth() + 1));
+      let MONTH = 1000 * 60 * 60 * 24 * 30;
+      let currentMonth = new Date().getMonth();
+      let _month = Math.floor(this.diff / MONTH) - currentMonth;
+      if (currentMonth > this.selectedDate.month) {
+        _month--;
+      }
+      return this.format(_month);
     },
     weeksPassed() {
       return this.format(Math.floor(this.diff / (1000 * 60 * 60 * 24 * 7)));
@@ -167,53 +152,33 @@ export default {
     },
     secondsPassed() {
       return this.format(Math.floor(this.diff / 1000));
+    },
+    nextBirthday() {
+      let date = new Date();
+      let nextBirthDay = new Date(
+        date.getFullYear(),
+        this.selectedDate.month,
+        this.selectedDate.day
+      );
+      let next = Math.round((nextBirthDay - date) / (24 * 60 * 60 * 1000));
+      if (next < 0) {
+        next = 365 - Math.abs(next);
+      }
+      return next;
     }
   },
   watch: {
-    year: function() {
-      this.birthdate = new Date(
-        this.year,
-        this.month,
-        this.day,
-        this.hour,
-        this.minute
-      );
-    },
-    month: function() {
-      this.birthdate = new Date(
-        this.year,
-        this.month,
-        this.day,
-        this.hour,
-        this.minute
-      );
-    },
-    day: function() {
-      this.birthdate = new Date(
-        this.year,
-        this.month,
-        this.day,
-        this.hour,
-        this.minute
-      );
-    },
-    hour: function() {
-      this.birthdate = new Date(
-        this.year,
-        this.month,
-        this.day,
-        this.hour,
-        this.minute
-      );
-    },
-    minute: function() {
-      this.birthdate = new Date(
-        this.year,
-        this.month,
-        this.day,
-        this.hour,
-        this.minute
-      );
+    selectedDate: {
+      handler: function() {
+        this.birthdate = new Date(
+          this.selectedDate.year,
+          this.selectedDate.month,
+          this.selectedDate.day,
+          this.selectedDate.hour,
+          this.selectedDate.minute
+        );
+      },
+      deep: true
     }
   }
 };
@@ -222,6 +187,11 @@ export default {
 <style>
 #details {
   font-size: 1.25rem;
+  border-radius: 1.5rem;
+}
+
+p {
+  margin-bottom: 0.5rem;
 }
 
 p > span {
